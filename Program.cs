becb4224ea291842df0580c3e39cf5e0a5c2d08c
Matix97 <mateusz_baranski@outlook.com>
2019-06-04 23:16:40 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DotNetCoreSqlDb {
+    public class Program {
+        // public static void Main(string[] args)
+        // {
+        //     BuildWebHost(args).Run();
+        //    //  CreateWebHostBuilder(args).Build().Run();
+        // }
+
+        // public static IWebHost BuildWebHost(string[] args) =>
+        //     WebHost.CreateDefaultBuilder(args)
+        //         .UseStartup<Startup>()
+        //         .Build();
+        public static void Main (string[] args)
+
+        {
+
+            var host = new WebHostBuilder ()
+                .UseKestrel ()
+                .UseContentRoot (Directory.GetCurrentDirectory ())
+                .UseIISIntegration ()
+                .UseStartup<Startup> ()
+                .Build ();
+
+            host.Run ();
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreSqlDb.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Microsoft.AspNetCore.Http;
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Authentication;
+
+using Microsoft.AspNetCore.StaticFiles;
+
+using System.Threading;
+
+
+
+namespace DotNetCoreSqlDb {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+            // Add framework services.
+            services.AddMvc ();
+
+            // services.AddDbContext<MyDatabaseContext>(options =>
+            //        options.UseSqlite("Data Source=localdatabase.db"));
+            // Use SQL Database if in Azure, otherwise, use SQLite
+            if (Environment.GetEnvironmentVariable ("ASPNETCORE_ENVIRONMENT") == "Production")
+                services.AddDbContext<MyDatabaseContext> (options =>
+                    options.UseSqlServer (Configuration.GetConnectionString ("MyDbConnection")));
+            else
+                services.AddDbContext<MyDatabaseContext> (options =>
+                    options.UseSqlite ("Data Source=localdatabase.db"));
+
+            // Automatically perform database migration
+            services.BuildServiceProvider ().GetService<MyDatabaseContext> ().Database.Migrate ();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+            loggerFactory.AddConsole (Configuration.GetSection ("Logging"));
+            loggerFactory.AddDebug ();
+            loggerFactory.AddAzureWebAppDiagnostics ();
+
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+                app.UseBrowserLink ();
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
+            }
+        
+#if NoOptions
+
+            #region UseWebSockets
+
+            app.UseWebSockets();
+
+            #endregion
+
+#endif
+
+#if UseOptions
+
+            #region UseWebSocketsOptions
+
+            var webSocketOptions = new WebSocketOptions()
+
+            {
+
+                KeepAliveInterval = TimeSpan.FromSeconds(10),
+
+                ReceiveBufferSize = 4 * 1024
+
+            };
+
+            app.UseWebSockets(webSocketOptions);
+
+            #endregion
+
+#endif
+#region AcceptWebSocket
+
+            app.Use(async (context, next) =>
+
+            {
+
+                if (context.Request.Path == "/ws")
+
+                {
+
+                    if (context.WebSockets.IsWebSocketRequest)
+
+                    {
+
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                        await Echo(context, webSocket);
+
+                    }
+
+                    else
+
+                    {
+
+                        context.Response.StatusCode = 400;
+
+                    }
+
+                }
+
+                else
+
+                {
+
+                    await next();
+
+                }
+
+
+
+            });
+
+#endregion
+
+            app.UseFileServer();
+
+      
+            app.UseStaticFiles ();
+
+            app.UseMvc (routes => {
+                routes.MapRoute (
+                    name: "default",
+                    template: "{controller=Todos}/{action=Index}/{id?}");
+            });
+            app.UseMvc ();
+        }
+        
+#region Echo
+ private async Task Echo(HttpContext context, WebSocket webSocket)
+
+        {
+
+            var buffer = new byte[1024 * 4];
+
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer),CancellationToken.None);
+
+            while (!result.CloseStatus.HasValue)
+
+            {
+
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            }
+
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription,CancellationToken.None);
+
+        }
+#endregion
+    }
+
+}  
